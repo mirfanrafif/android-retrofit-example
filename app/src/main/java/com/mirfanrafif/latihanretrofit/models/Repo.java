ae5a1040bfdc6a0117c04c32ad4a55b094f17688
Irfan Rafif <mirfanrafif17@gmail.com>
2020-11-30 14:09:40 +0700

    selesai

---
+++ app/src/main/java/com/mirfanrafif/latihanretrofit/models/Repo.java
@@ -0,0 +1,40 @@
+package com.mirfanrafif.latihanretrofit.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Repo {
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("html_url")
+    private String htmlUrl;
+    @SerializedName("description")
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
